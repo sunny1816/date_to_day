24d6e87c2f4929712195e9b851b7e135e2f82ebb
sunny1816 <sunny5880kumar@gmail.com>
2025-02-15 22:03:50 +0530

    Delete mobile directory

--- mobile/src/utils/dateUtils.js
+++
@@ -1,7 +0,0 @@
-export const formatDate = (text) => {
-    let newText = text.replace(/[^0-9]/g, "");
-    if (newText.length > 4) newText = newText.slice(0, 4) + "-" + newText.slice(4);
-    if (newText.length > 7) newText = newText.slice(0, 7) + "-" + newText.slice(7);
-    return newText;
-  };
-  
