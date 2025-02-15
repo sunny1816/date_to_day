a098e53fa896e17c5b91a4106bc580e1396f5413
sunny1816 <sunny5880kumar@gmail.com>
2025-02-15 21:47:45 +0530

    Delete backend directory

--- backend/controllers/datecontroller.js
+++
@@ -1,7 +0,0 @@
-exports.getDayOfWeek = (req, res) => {
-    const { date } = req.body;
-    if (!date) return res.status(400).json({ error: "Date is required" });
-
-    const day = new Date(date).toLocaleDateString("en-US", { weekday: "long" });
-    res.json({ date, day });
-};
--- backend/models/usermodel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const UserSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    createdAt: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model("User", UserSchema);
